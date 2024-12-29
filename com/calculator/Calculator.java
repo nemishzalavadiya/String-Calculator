0c4719496f321cc04aa56aea03b5be3009660caf
Nemish Zalavadiya <13101999znemish@gmail.com>
2024-12-29 20:36:51 +0530

    Added cmd to be ran in main class file.

--- com/calculator/Calculator.java
+++ com/calculator/Calculator.java
@@ -5,6 +5,8 @@ import com.calculator.service.test.TestServiceImpl;
 
 public class Calculator {
   public static void main(String[] args) {
+    // execute below cmd for testing this suite
+    // java -classpath ./out/production/String-Calculator com.calculator.Calculator
     TestService testService = new TestServiceImpl();
     testService.runTests();
   }
